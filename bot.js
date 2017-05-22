0cfc56d82112acd24c7d04543271012d5834a9b8
smironescu <sergio.mironescu@marfeel.com>
2017-05-22 23:46:51 +0200

    get method tweet

--- bot.js
+++ bot.js
@@ -1,6 +1,25 @@
+/* TWEET BOT made with node.js - simply app
+  Author: Sergio M.
+  */
 
-console.log('this is working!')
-const Twit = require('twit')
-const config = require('./config')
+//console.log('It works!!')
 
-const T = new Twit(config)
+const Twit = require('twit'),
+config = require('./config'),
+T = new Twit(config);
+
+const gotData = (err, data, response) => {
+  const tweets = data.statuses;
+
+  for(var i = 0; i < tweets.length; i++){
+      const tweet = tweets[i];
+        console.log(tweet.text)
+  }
+};
+
+const params = {
+  q: 'rainbow',
+  count: 3
+};
+
+T.get('search/tweets', params, gotData)
